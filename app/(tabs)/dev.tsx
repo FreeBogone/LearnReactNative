dc049e06b485479aeee2c3c6f3207d1343967a56
Bowen Truelove <botruelove2002@gmail.com>
2024-09-25 11:36:45 -0500

    Creating Dev Page

--- app/(tabs)/dev.tsx
+++ app/(tabs)/dev.tsx
@@ -6,17 +6,17 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native';
+
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      {/* this is where I will test components*/}
-    </ParallaxScrollView>
+    <SafeAreaView>
+        <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Dev</ThemedText>
+        <ThemedText type="subtitle">Testing Page for Components</ThemedText>
+        </ThemedView>
+    </SafeAreaView>
   );
 }
 
