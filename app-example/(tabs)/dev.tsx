8fe0300ac24627a6595bea8e62e44225d3cd1cf2
Bowen Truelove <botruelove2002@gmail.com>
2024-10-03 13:15:24 -0500

    Reseting project

--- app-example/(tabs)/dev.tsx
+++ app-example/(tabs)/dev.tsx
@@ -1,5 +1,6 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, View } from 'react-native';
+import React from 'react';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -7,17 +8,26 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { SafeAreaView } from 'react-native';
+import Card from '@/components/Card';
 
 
 export default function TabTwoScreen() {
+  const dataSet = [{name: "Bob", number: 1345},
+                {name: "Joe", number: 5490},
+                {name: "Jerry", number: 3401}]
+
+  const makeCards = () => {
+    return dataSet.map((data) => <Card data={data}/>)
+  }
+
   return (
     <SafeAreaView>
-        <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Dev</ThemedText>
-        <ThemedText type="subtitle">Testing Page for Components</ThemedText>
-        </ThemedView>
+      <View style={styles.stepContainer}>
+        {makeCards()}
+      </View>
     </SafeAreaView>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -27,8 +37,18 @@ const styles = StyleSheet.create({
     left: -35,
     position: 'absolute',
   },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
   },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
